20e91560e050b8b4941273d3062ff5a2b693b760
Artem <artjom310@gmail.com>
2021-09-04 12:56:15 +0300

    marix_max_value

--- stepik1.py
+++ stepik1.py
@@ -1,14 +1,24 @@
-a = [[1, 2, 3, 4],
-     [5, 6, 3, 15],
-     [0, 2, 3, 1],
-     [5, 2, 8, 5]]
-for i in range(len(a)):
-    arf = sum(a[i]) / len(a[i])
-    k = 0
-    for j in range(len(a)):
-        if a[i][j] > arf:
-            k += 1
-    print(k)
+n = int(input())
+m = int(input())
+matrix = []
+for i in range(n):
+    temp = [int(num) for num in input().split()]
+    matrix.append(temp)
 
-'''Подсчет количества элементов
- в подсписках > среднего арф строки'''
+b_i, b_j = 0, 0
+max_v = matrix[0][0]
+for i in range(len(matrix)):
+    for j in range(m):
+        if matrix[i][j] > max_v:
+            max_v = matrix[i][j]
+            b_i = i
+            b_j = j
+print(b_i, b_j)
+'''6
+8
+-4 -3 -4 -4 -1 -8 -2 -3
+-2 -3 -9 -7 -3 -4 -4 -5
+-4 -3 -4 -4 -1 -2 -2 -3
+-2 -3 -9 -3 -3 -1 -4 -5
+-5 -3 -4 -4 -1 -2 -2 -3
+-2 -3 -7 -8 -3 -4 -4 -5'''
