7007895ad98cc51e4d5169153487622b681331fe
Artem <artjom310@gmail.com>
2021-09-06 20:58:06 +0300

    замена диагоналей местами

--- lstcomp.py
+++ lstcomp.py
@@ -1,19 +1,11 @@
-#  алгоритм проверяет симметричная ли квадратная матрица
-#  относительно главной диагонали
-#  выводит да или нет
+'''Поменять местами главную и побочную диаггонали'''
 n = int(input())
 a = []
 res = []
 for i in range(n):
     a.append(list(map(int, input().split())))
 for i in range(len(a)):
-    for j in range(len(a[i])):
-        if i != j:
-            if a[i][j] == a[j][i]:
-                res.append('YES')
-            else:
-                res.append('NO')
-if 'NO' in res:
-    print('NO')
-else:
-    print('YES')
+    a[i][i], a[n-i-1][i] = a[n-i-1][i], a[i][i]
+print()
+for i in a:
+    print(*i)
