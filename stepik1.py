39c60cd43454c8ad21a3bc90b73db3d3458f26e4
Artem <artjom310@gmail.com>
2021-10-02 18:22:02 +0300

    matrix reshape order=F

--- stepik1.py
+++ stepik1.py
@@ -1,10 +1,6 @@
-'''На вход программе подаются два натуральных числа nn и mm. Напишите программу, 
-которая создает матрицу размером n на mn×m и 
-заполняет её числами от 1 до n * m в соответствии с образцом.'''
-
+import numpy as np
 
 n, m = list(map(int, input().split()))
-
 matrix = []
 g = m
 a = 1
@@ -12,7 +8,23 @@ for i in range(n):
     matrix.append(list(range(a, g + 1)))
     a = g + 1
     g += m
-for i in range(n):
+
+
+def transpose(matr):
+    res = []
+    n = len(matr)
+    m = len(matr[0])
     for j in range(m):
-        print(str(matrix[i][j]).ljust(2), end=' ')
-    print()
+        tmp = []
+        for i in range(n):
+            tmp = tmp+[matr[i][j]]
+        res = res+[tmp]
+    return res
+
+
+tr_matrix = transpose(matrix)
+m1 = np.array(tr_matrix)
+m_res = np.reshape(m1, (n, m), order='F')
+for i in m_res:
+    print(*i)
+print()
