4526d8c62474da55201197f6dfe55db9f975b111
Artem <artjom310@gmail.com>
2021-10-08 23:47:59 +0300

    postman_solve

--- stepik1.py
+++ stepik1.py
@@ -1,30 +1,6 @@
-import numpy as np
-
-n, m = list(map(int, input().split()))
-matrix = []
-g = m
-a = 1
+n = int(input())
+matrix = [[0] * n for _ in range(n)]
 for i in range(n):
-    matrix.append(list(range(a, g + 1)))
-    a = g + 1
-    g += m
-
-
-def transpose(matr):
-    res = []
-    n = len(matr)
-    m = len(matr[0])
-    for j in range(m):
-        tmp = []
-        for i in range(n):
-            tmp = tmp+[matr[i][j]]
-        res = res+[tmp]
-    return res
-
-
-tr_matrix = transpose(matrix)
-m1 = np.array(tr_matrix)
-m_res = np.reshape(m1, (n, m), order='F')
-for i in m_res:
-    print(*i)
-print()
+    matrix[i][i] = 1
+    matrix[i][n - i - 1] = 1
+    print(*matrix[i])
