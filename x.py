77fae50b37706f03760b468ade65f1c30a43d0c2
Artem <artjom310@gmail.com>
2021-10-18 15:43:04 +0300

    matrix fakamaza

--- x.py
+++ x.py
@@ -1,17 +1,19 @@
-n = int(input())
-matrix = [[1] * n for _ in range(n)]
+'''Sample Input 1:
 
-'''
-1  1  1  1  1
-0  1  1  1  0
-0  0  1  0  0
-0  1  1  1  0
-1  1  1  1  1'''
+5 5
+Sample Output 1:
 
+1 2 3 4 5
+2 3 4 5 1
+3 4 5 1 2
+4 5 1 2 3
+5 1 2 3 4'''
+
+
+n, m = list(map(int, input().split()))
+matrix = [[0] * m for _ in range(n)]
 for i in range(n):
-    for j in range(n):
-        if i > j and i < n-1-j or i < j and i > n-1-j:
-            matrix[i][j] = 0
-
+    for j in range(m):
+        matrix[i][j] = (i + j) % m + 1
 for i in matrix:
     print(*i)
