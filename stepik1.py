bdfa3cac2337affc75dc1ba04ad295e46f1cd957
Artem <artjom310@gmail.com>
2021-11-26 17:14:45 +0300

    встречалось ли число раньше

--- stepik1.py
+++ stepik1.py
@@ -1,9 +1,18 @@
-# Напишите программу для вывода количества уникальных символов каждого считанного слова без учета регистра.
-n = int(input())
-lst = []
-for i in range(n):
-    s = input()
-    s = s.lower()
-    lst.append(set(s))
-for i in lst:
-    print(len(i))
+# На вход программе подается строка текста, содержащая числа.
+# Для каждого числа выведите слово YES (в отдельной строке),
+# если это число ранее встречалось в последовательности или NO,
+# если не встречалось.
+s = input().split()
+leadingremoved = []
+for i in s:
+    while i[0] == "0":
+        i = i[1:]
+    leadingremoved.append(i)
+a = set()
+for i in leadingremoved:
+    if i in a:
+        print('YES')
+        a.add(i)
+    else:
+        print('NO')
+        a.add(i)
