a6e0a25f530ec4f33fd78215fe94a60ae3a45639
Artem <artjom310@gmail.com>
2021-12-21 16:12:08 +0300

    work with dicts

--- stepik1.py
+++ stepik1.py
@@ -1,18 +1,6 @@
-# На вход программе подается строка текста, содержащая числа.
-# Для каждого числа выведите слово YES (в отдельной строке),
-# если это число ранее встречалось в последовательности или NO,
-# если не встречалось.
-s = input().split()
-leadingremoved = []
-for i in s:
-    while i[0] == "0":
-        i = i[1:]
-    leadingremoved.append(i)
-a = set()
-for i in leadingremoved:
-    if i in a:
-        print('YES')
-        a.add(i)
-    else:
-        print('NO')
-        a.add(i)
+digits = {'0': 'zero', '1': 'one', '2': 'two', '3': 'three', '4': 'four',
+          '5': 'five', '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine'
+          }
+a = list(input())
+for i in a:
+    print(digits[i], end=' ')
