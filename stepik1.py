9f7dbdff4408da033061eaad74dede9950530cd8
Artem <artjom310@gmail.com>
2022-01-11 22:28:01 +0300

    словари с синонимами зеркально

--- stepik1.py
+++ stepik1.py
@@ -1,29 +1,23 @@
-'''На вход программе подаются два предложения.
-Напишите программу, которая определяет, являются они анаграммами или нет.
-Ваша программа должна игнорировать регистр символов,
-знаки препинания и пробелы.'''
+'''words = {}
+for _ in range(int(input())):
+    a, b = input().split()
+    words[a], words[b] = b, a
+print(words[input()])'''
 
+n = int(input())
+result = {}
+res1 = {}
+keys = []
+values = []
+for i in range(n):
+    a = input().split()
+    keys.append(a[1])
+    values.append(a[0].strip())
 
-#  re.sub(u'[^\w\d\s]+', '', str())   words = re.sub(r'[.,;:-?-!]', '', input())
-a = [i.strip('.,!?:;-') for i in input().lower().strip()]
-b = [i.strip('.,!?:;-') for i in input().lower().strip()]
-# rint(a, b, sep='\n')
-d = {}
-d1 = {}
-for i in a:
-    if i.isalpha():
-        d[i] = d.get(i, 0) + 1
-    else:
-        continue
-
-for i in b:
-    if i.isalpha():
-        d1[i] = d1.get(i, 0) + 1
-    else:
-        continue
-
-if d == d1:
-    print("YES")
+result = dict(zip(keys, values))
+res1 = dict(zip(values, keys))
+m = input()
+if result.get(m) is not None:
+    print(result.get(m))
 else:
-    print('NO')
-# print(d, d1, sep='\n')
+    print(res1.get(m))
