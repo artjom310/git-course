3aecbce96c092b9e63b8c86de2db46ab4f91ef4c
Artem <artjom310@gmail.com>
2022-01-12 22:07:21 +0300

    города и страны

--- stepik1.py
+++ stepik1.py
@@ -1,23 +1,2 @@
-'''words = {}
-for _ in range(int(input())):
-    a, b = input().split()
-    words[a], words[b] = b, a
-print(words[input()])'''
-
-n = int(input())
-result = {}
-res1 = {}
-keys = []
-values = []
-for i in range(n):
-    a = input().split()
-    keys.append(a[1])
-    values.append(a[0].strip())
-
-result = dict(zip(keys, values))
-res1 = dict(zip(values, keys))
-m = input()
-if result.get(m) is not None:
-    print(result.get(m))
-else:
-    print(res1.get(m))
+a = ['Германия', 'Берлин' 'Мюнхен', 'Гамбург', 'Дортмунд']
+print(a[1:])
--- x.py
+++ x.py
@@ -1,2 +1,27 @@
-a = [i.strip('.,!?:;-') for i in input().lower()]
-print(a)
+'''2
+d = {}
+for _ in range(int(input())):
+    country, *cities = input().split()
+    d.update(dict.fromkeys(cities, country))
+for _ in range(int(input())):    
+    print(d[input()])
+'''
+
+
+n = int(input())
+keys = []
+values = []
+for i in range(n):
+    a = input().split()
+    keys.append(a[0])
+    values.append(a[1:])
+d = {}
+for i in range(len(keys)):
+    a = d.fromkeys(values[i], keys[i])
+    d.update(a)
+m = int(input())
+b = []
+for i in range(m):
+    b.append(input().strip())
+for i in b:
+    print(d.get(i))
