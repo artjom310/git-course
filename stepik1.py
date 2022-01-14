1cfca93904fe1662fb22b6271d9720054e4e2411
Artem <artjom310@gmail.com>
2022-01-14 11:43:06 +0300

    телефонная книга

--- stepik1.py
+++ stepik1.py
@@ -1,2 +1,22 @@
-a = ['Германия', 'Берлин' 'Мюнхен', 'Гамбург', 'Дортмунд']
-print(a[1:])
+'''
+Телефонная книга
+dct = {}
+for _ in range(int(input())):
+    phone, name = input().lower().split()
+    dct.setdefault(name, []).append(phone)
+for _ in range(int(input())):
+    print(*dct.get(input().lower(), ['абонент не найден']))'''
+
+d = {}
+for i in range(int(input())):
+    a = input().lower().split()
+    d.setdefault(a[1], []).append(a[0])
+lst = []
+for i in range(int(input())):
+    b = input().lower()
+    lst.append(b)
+for i in lst:
+    try:
+        print(*d.get(i))
+    except:
+        print('абонент не найден')
