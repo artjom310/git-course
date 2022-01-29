81e2d9afc2d1d6f687b8a8aba2abc474eee345de
Artem Frolov <artjom310@gmail.com>
2022-01-29 16:26:50 +0300

    расшифровка методом частотного анализа

--- stepik1.py
+++ stepik1.py
@@ -1,22 +1,29 @@
-'''
-Телефонная книга
-dct = {}
-for _ in range(int(input())):
-    phone, name = input().lower().split()
-    dct.setdefault(name, []).append(phone)
-for _ in range(int(input())):
-    print(*dct.get(input().lower(), ['абонент не найден']))'''
+'''Sample Input 1:
 
+*!*!*?
+3
+а: 3
+н: 2
+с: 1
+'''
+a = input()
+n = int(input())
 d = {}
-for i in range(int(input())):
-    a = input().lower().split()
-    d.setdefault(a[1], []).append(a[0])
-lst = []
-for i in range(int(input())):
-    b = input().lower()
-    lst.append(b)
-for i in lst:
-    try:
-        print(*d.get(i))
-    except:
-        print('абонент не найден')
+for i in a:
+    d[i] = d.get(i, 0) + 1
+# print(d)  # {'*': 3, '!': 2, '?': 1}
+
+d1 = {}
+
+keys = []
+values = []
+for i in range(n):
+    i = input().split(': ')
+    keys.append(int(i[1]))
+    values.append(i[0])
+d1 = dict(zip(keys, values))  # {'3': 'а', '2': 'н', '1': 'с'}
+# print(d1)
+s = []
+for i in a:
+    s.append(d1.get(d.get(i)))
+print(''.join(s))
