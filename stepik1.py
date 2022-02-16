9bdd2f9219d958ba9fa0f956740c2c9dd4775c3c
Artem Frolov <artjom310@gmail.com>
2022-02-16 21:05:56 +0300

    рнд наш единственный бог

--- stepik1.py
+++ stepik1.py
@@ -1,16 +1,27 @@
-from pprint import pprint
+'''ddgd
+'''
+
+import random
+
+addres = 'AB23_56VG'
+
+"""AB23_56VG
+"""
+
+# функция для рандомного создания индекса
 
 
-from pprint import pprint
-student_ids = ['S001', 'S002', 'S003', 'S004', 'S005', 'S006',
-               'S007', 'S008', 'S009', 'S010', 'S011', 'S012', 'S013']
-student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', 'Batista Cesare', 'Francesco Totti',
-                 'Khalid Hussain', 'Ethan Hawke', 'David Bowman', 'James Milner', 'Michael Owen', 'Gary Oldman', 'Tom Hardy']
-student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
+def generate_index():
+    '''гребана пайлинт сцуко'''
+    a = random.randint(65, 90)
+    b = random.randint(65, 90)
+    c = random.randint(65, 90)
+    d = random.randint(65, 90)
+    n = random.randrange(9)
+    n1 = random.randrange(9)
+    n2 = random.randrange(9)
+    n3 = random.randrange(9)
+    return chr(a) + chr(b) + str(n) + str(n1) + '_' + str(n2)  + str(n3) + chr(c) + chr(d)
 
-result = [{student_ids[i]: {student_names[i]: student_grades[i]}} for i in range(len(student_ids))]
-print(pprint(result))
-d = [{'S001': {'Camila Rodriguez': 93}}, {'S002': {'Juan Cruz': 93}}, {'S003': {'Dan Richards': 93}}, {'S004': {'Sam Boyle': 93}}, {'S005': {'Batista Cesare': 
-93}}, {'S006': {'Francesco Totti': 93}}, {'S007': {'Khalid Hussain': 93}}, {'S008': {'Ethan Hawke': 93}}, {'S009': {'David Bowman': 93}}, {'S010': {'James Milner': 93}}, {'S011': {'Michael Owen': 93}}, {'S012': {'Gary Oldman': 93}}, {'S013': {'Tom Hardy': 93}}]
 
-result = [{x: {y: z}} for x, y, z in zip(student_ids, student_names, student_grades)]
+print(generate_index())
