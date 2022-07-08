0bae23e2afe73a7a82b6c26dce7c6302d2679f6a
Artem Frolov <artjom310@gmail.com>
2022-07-08 08:31:37 +0300

    16.3 Часть 3

--- x.py
+++ x.py
@@ -1,20 +1,10 @@
-def add3(x):
-    return x + 3
+
+def concat(*data, sep=' '):
+    return f'{sep}'.join(map(str, data))
 
 
-def mul7(x):
-    return x * 7
-
-
-def func_apply(fn, item):
-    return list(map(fn, item))
-
-
-print(func_apply(mul7, [1, 2, 3, 4, 5, 6]))
-print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
-print(func_apply(str, [1, 2, 3, 4, 5, 6]))
-
-'''[7, 14, 21, 28, 35, 42]
-[4, 5, 6, 7, 8, 9]
-['1', '2', '3', '4', '5', '6']
-'''
+print(concat('hello', 'python', 'and', 'stepik'))
+print(concat('hello', 'python', 'and', 'stepik', sep='*'))
+print(concat('hello', 'python', sep='()()()'))
+print(concat('hello', sep='()'))
+print(concat(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='$$'))
