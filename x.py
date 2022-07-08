9a58975f1c8572e075fea4bfb7115ab62d16bcb3
Artem Frolov <artjom310@gmail.com>
2022-07-08 14:27:11 +0300

    16.3 Часть 3 жарим дальше ф-ии

--- x.py
+++ x.py
@@ -1,10 +1,8 @@
-
-def concat(*data, sep=' '):
-    return f'{sep}'.join(map(str, data))
+words = 'the world is mine take a look what you have started'.split()
 
 
-print(concat('hello', 'python', 'and', 'stepik'))
-print(concat('hello', 'python', 'and', 'stepik', sep='*'))
-print(concat('hello', 'python', sep='()()()'))
-print(concat('hello', sep='()'))
-print(concat(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='$$'))
+def concat(*args):
+    '''f' {delimiter} '.join(map(str, *data))'''
+    return list(map(lambda x: '"' + str(x) + '"', *args))
+
+print(*concat(words))
--- y_lab_homework.py
+++ y_lab_homework.py
@@ -1,58 +1,8 @@
-'''  ------------------------------ 
-| 1 | 2 | 10 | 23 | 123 | 3000 |
- ------------------------------ 
- ------------------------- 
-| abc | def | ghi | 12345 |
- ------------------------- 
- ***************** 
-| abc | def | ghi |
- ***************** 
- ----------------- 
-# abc # def # ghi #
- ----------------- 
- ***************** 
-# abc # def # ghi #
- ***************** 
+from functools import reduce
 
 
- ------------------------------ 
-| 1 | 2 | 10 | 23 | 123 | 3000 |
- ------------------------------ 
+def product_of_odds(*data):
+    '''ф-ия перемножает нечетные числа'''
+    return reduce(lambda x, y: x * y, filter(lambda x: x % 2, data), 1)
 
- ------------------------- 
-| abc | def | ghi | 12345 |
- ------------------------- 
-
- *****************
-| abc | def | ghi |        
- *****************
-
- -----------------
-# abc # def # ghi #
- -----------------
-
- *****************
-# abc # def # ghi #
- *****************
-
- '''
-
-
-def pretty_print(*data, side='-', delimiter='|'):
-    
-    s = f' {delimiter} '.join(map(str, *data))
-    c = 2
-    for _ in s:
-        c += 1
-    a = f"{side}" * c
-    return  f'''
- {a} 
-{delimiter} {s} {delimiter}
- {a} '''
-
-
-print(pretty_print([1, 2, 10, 23, 123, 3000]))
-print(pretty_print(['abc', 'def', 'ghi', '12345']))
-print(pretty_print(['abc', 'def', 'ghi'], side='*'))
-print(pretty_print(['abc', 'def', 'ghi'], delimiter='#'))
-print(pretty_print(['abc', 'def', 'ghi'], side='*', delimiter='#'))
+print(product_of_odds(1,2,3,4,5))
