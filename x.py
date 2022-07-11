a7b02fb2202cae78254613554630efa0208e3ff0
Artem Frolov <artjom310@gmail.com>
2022-07-11 18:15:05 +0300

    завершил блок с ф-ями

--- x.py
+++
@@ -1,8 +0,0 @@
-words = 'the world is mine take a look what you have started'.split()
-
-
-def concat(*args):
-    '''f' {delimiter} '.join(map(str, *data))'''
-    return list(map(lambda x: '"' + str(x) + '"', *args))
-
-print(*concat(words))
--- y_lab_homework.py
+++ y_lab_homework.py
@@ -1,8 +1,43 @@
-from functools import reduce
+'''
+ ------------------------------ 
+| 1 | 2 | 10 | 23 | 123 | 3000 |
+ ------------------------------ 
+ ------------------------- 
+| abc | def | ghi | 12345 |
+ ------------------------- 
+ ***************** 
+| abc | def | ghi |
+ ***************** 
+ ----------------- 
+# abc # def # ghi #
+ ----------------- 
+ ***************** 
+# abc # def # ghi #
+ ***************** 
+
+'''
 
 
-def product_of_odds(*data):
-    '''ф-ия перемножает нечетные числа'''
-    return reduce(lambda x, y: x * y, filter(lambda x: x % 2, data), 1)
+def pretty_print(*data, side='-', delimiter='|'):
 
-print(product_of_odds(1,2,3,4,5))
+    s = f' {delimiter} '.join(map(str, *data))
+    c = 2
+    for _ in s:
+        c += 1
+    a = f"{side}" * c
+    print(f'''
+ {a} 
+{delimiter} {s} {delimiter}
+ {a} ''', end='')
+
+
+pretty_print([1, 2, 10, 23, 123, 3000])
+pretty_print(['abc', 'def', 'ghi', '12345'])
+pretty_print(['abc', 'def', 'ghi'], side='*')
+pretty_print(['abc', 'def', 'ghi'], delimiter='#')
+pretty_print(['abc', 'def', 'ghi'], side='*', delimiter='#')
+
+
+PH = r'C:\Users\User\Desktop\eng\back_dev\git-course\image.JPG'
+with open(PH, 'rb') as r:
+    print(r.readline())
