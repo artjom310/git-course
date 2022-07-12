e5fd4bcf4883636f923242219d3e0751c545310a
Artem Frolov <artjom310@gmail.com>
2022-07-12 09:16:29 +0300

    17.2 Работа с текстовыми файлами. Часть 1

--- y_lab_homework.py
+++ y_lab_homework.py
@@ -1,43 +1,5 @@
-'''
- ------------------------------ 
-| 1 | 2 | 10 | 23 | 123 | 3000 |
- ------------------------------ 
- ------------------------- 
-| abc | def | ghi | 12345 |
- ------------------------- 
- ***************** 
-| abc | def | ghi |
- ***************** 
- ----------------- 
-# abc # def # ghi #
- ----------------- 
- ***************** 
-# abc # def # ghi #
- ***************** 
-
-'''
-
-
-def pretty_print(*data, side='-', delimiter='|'):
-
-    s = f' {delimiter} '.join(map(str, *data))
-    c = 2
-    for _ in s:
-        c += 1
-    a = f"{side}" * c
-    print(f'''
- {a} 
-{delimiter} {s} {delimiter}
- {a} ''', end='')
-
-
-pretty_print([1, 2, 10, 23, 123, 3000])
-pretty_print(['abc', 'def', 'ghi', '12345'])
-pretty_print(['abc', 'def', 'ghi'], side='*')
-pretty_print(['abc', 'def', 'ghi'], delimiter='#')
-pretty_print(['abc', 'def', 'ghi'], side='*', delimiter='#')
-
-
-PH = r'C:\Users\User\Desktop\eng\back_dev\git-course\image.JPG'
-with open(PH, 'rb') as r:
-    print(r.readline())
+ph = 'numbers.txt'
+with open(ph, 'r', encoding='utf-8') as f:
+    a = f.read()
+    new_str = '\n'.join(el.strip() for el in a.split('\n'))
+    print(sum(list(map(int, new_str.split()))))
